9fb4775f138cea9c806f9ea4d739cba37399a9a9
mailmunch <mailmunch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-04 13:15:06 +0000

    fixed the bug where user gets signed out - version 1.0.4
    git-svn-id: https://plugins.svn.wordpress.org/constant-contact-forms-by-mailmunch/trunk@1059505 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- constantcontact-mailmunch.php
+++ constantcontact-mailmunch.php
@@ -3,7 +3,7 @@
   Plugin Name: Constant Contact Forms by MailMunch
   Plugin URI: http://www.mailmunch.co
   Description: The ConstantContact plugin allows you to quickly and easily add signup forms for your ConstantContact lists. Popup, Embedded, Top Bar and a variety of different options available.
-  Version: 1.0.3
+  Version: 1.0.4
   Author: MailMunch
   Author URI: http://www.mailmunch.co
   License: GPL2
@@ -14,7 +14,7 @@
   require_once( plugin_dir_path( __FILE__ ) . 'inc/sidebar_widget.php' );
 
   define( 'CONSTANTCONTACT_MAILMUNCH_SLUG', "constantcontact-mailmunch");
-  define( 'CONSTANTCONTACT_MAILMUNCH_VER', "1.0.3");
+  define( 'CONSTANTCONTACT_MAILMUNCH_VER', "1.0.4");
   define( 'CONSTANTCONTACT_MAILMUNCH_URL', "www.mailmunch.co");
 
   // Create unique WordPress instance ID
@@ -322,7 +322,7 @@
         <div class="form-group">
           <input type="hidden" name="action" value="save_settings" />
 
-          <select name="mailmunch_data[site_id]">
+          <select name="cc_mm_data[site_id]">
             <?php foreach ($sites as $site) { ?>
             <option value="<?php echo $site->id ?>"><?php echo $site->name ?></option>
             <?php } ?>
